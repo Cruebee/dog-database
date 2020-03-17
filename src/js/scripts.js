8ffd4a08cdcbb4ff200e9c9af1ece3b7ff3a6d58
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-03-17 15:43:31 -0600

    run lint and convert with prettier

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
 // JS sheet
 // create IIFE
-var pokemonRepository = (function () {
+var pokemonRepository = (function() {
   var repository = [];
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=50';
   var $pokemonList = $('.pokemon-list');
@@ -8,43 +8,58 @@ var pokemonRepository = (function () {
 
   // funcrtion to load a list of pokemon from API:
   function loadList() {
-    return $.ajax(apiUrl, {dataType: 'json'}).then(function(responseJSON) {
-      return responseJSON;
-    }).then(function(json) {
-      json.results.forEach(function(item) {
-        var pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
+    return $.ajax(apiUrl, { dataType: 'json' })
+      .then(function(responseJSON) {
+        return responseJSON;
+      })
+      .then(function(json) {
+        json.results.forEach(function(item) {
+          var pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function(e) {
+        /* eslint-disable no-console */
+        console.error(e);
+        /* eslint-enable no-console */
       });
-    }).catch(function(e) {
-      console.error(e);
-    })
   }
 
   // function to load details from API:
   function loadDetails(item) {
     var url = item.detailsUrl;
-    return $.ajax(url, {dataType: 'json'}).then(function (responseJSON) {
-      return responseJSON;
-    }).then(function (details) {
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      // Cleaner way to loop over pokemon types with map:
-      item.types = details.types.map(function(pokemon) {
-        return pokemon.type.name;
+    return $.ajax(url, { dataType: 'json' })
+      .then(function(responseJSON) {
+        return responseJSON;
+      })
+      .then(function(details) {
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        // Cleaner way to loop over pokemon types with map:
+        item.types = details.types.map(function(pokemon) {
+          return pokemon.type.name;
+        });
+      })
+      .catch(function(e) {
+        /* eslint-disable no-console */
+        console.error(e);
+        /* eslint-enable no-console */
       });
-
-    }).catch(function(e) {
-      console.error(e);
-    })
   }
 
   // function to create a list of pokemon from API:
   function addListItem(pokemon) {
-    var $listItem = $('<li class="list-group-item list-group-item-action"></li>');
-    var $listButton = $('<button id="showPokeModal" type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokeModalLong">' + pokemon.name + '</button>');
+    var $listItem = $(
+      '<li class="list-group-item list-group-item-action"></li>'
+    );
+    var $listButton = $(
+      '<button id="showPokeModal" type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokeModalLong">' +
+        pokemon.name +
+        '</button>'
+    );
 
     // append $listButton to  listItem and append $listItem to $pokemonList:
     $listItem.append($listButton);
@@ -63,16 +78,16 @@ var pokemonRepository = (function () {
     $('.modal-body')
       .empty()
       .append('<p>Height: ' + height + '</p>')
-      .append('<p>Types: ' + types+ '</p>')
+      .append('<p>Types: ' + types + '</p>')
       .append('<img src="' + imageUrl + '"></img>');
   }
 
   // add function to show details and update to show modal:
   function showDetails(item) {
-  pokemonRepository.loadDetails(item).then(function() {
-    showModalDetails(item.imageUrl, item.name, item.height, item.types);
-  });
-}
+    pokemonRepository.loadDetails(item).then(function() {
+      showModalDetails(item.imageUrl, item.name, item.height, item.types);
+    });
+  }
 
   // function to add pokemon:
   function add(creature) {
@@ -93,7 +108,6 @@ var pokemonRepository = (function () {
     showDetails: showDetails,
     showModalDetails: showModalDetails
   };
-
 })(); // end IIFE.
 
 // create list of pokemon buttons:
@@ -107,14 +121,14 @@ pokemonRepository.loadList().then(function() {
 $(document).ready(function() {
   $('#pokemon-search').on('keyup', function() {
     var value = $(this)
-    .val()
-    .toLowerCase()
+      .val()
+      .toLowerCase();
     $('#pokeSearchDiv *').filter(function() {
       $(this).toggle(
         $(this)
-        .text()
-        .toLowerCase()
-        .indexOf(value) > -1
+          .text()
+          .toLowerCase()
+          .indexOf(value) > -1
       );
     });
   });
